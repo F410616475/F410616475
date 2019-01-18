750d91e9e74173403f46aa49c8d1c8269fef9051
F410616475 <46801676+F410616475@users.noreply.github.com>
2019-01-18 11:36:39 +0800

    Add files via upload
    這是4.如何輸入左右鍵移動玩家?的成果

---
+++ Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class move : NetworkBehaviour {
+
+    // Use this for initialization
+   
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        
+            float h = Input.GetAxis("Horizomtal");
+        float v = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(h, 0, v);
+    
+}
+}
