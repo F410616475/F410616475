ccca2104248b46c97c8c6f7e80192832b33f8fba
F410616475 <46801676+F410616475@users.noreply.github.com>
2019-01-18 11:39:47 +0800

    Add files via upload
    這是6.將自己的玩家設為紅色。的成果

--- Assets/move.cs
+++ Assets/move.cs
@@ -6,16 +6,25 @@ using UnityEngine.Networking;
 public class move : NetworkBehaviour {
 
     // Use this for initialization
-   
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+
     }
 
     // Update is called once per frame
     void Update() {
 
-        
+        if (isLocalPlayer)
+        {
+
+
             float h = Input.GetAxis("Horizomtal");
-        float v = Input.GetAxis("Vertical");
-        transform.position = transform.position + new Vector3(h, 0, v);
-    
+            float v = Input.GetAxis("Vertical");
+            transform.position = transform.position + new Vector3(h, 0, v);
+        }
 }
 }
